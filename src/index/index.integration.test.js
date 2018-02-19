a6e733bd3c4ff0c6e9b0abe67ab0c9aa947cff40
Jason <jasonjones@salesforce.com>
2018-02-18 19:30:43 -0800

    Add db connection to base integration test

--- src/index/index.integration.test.js
+++ src/index/index.integration.test.js
@@ -2,10 +2,24 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 
 import app from '../config/app';
+import Config from '../config/config';
+import db from '../config/db';
+
+const env = process.env.NODE_ENV || "development";
+const config = Config[env];
 
 chai.use(chaiHttp);
+let dbConnection;
 
 describe('Index integration tests', () => {
+
+    before(() => {
+        dbConnection = db(config);
+    });
+
+    after(() => {
+        dbConnection.close();
+    });
 
     it('app is present', () => {
         expect(app).to.exist;
@@ -15,6 +29,8 @@ describe('Index integration tests', () => {
         return chai.request(app).get('/api').then(res => {
             expect(res).to.have.status(200);
             expect(res).to.be.json;
+            expect(res.body).to.have.property('version');
+            expect(res.body).to.have.property('message');
         });
     });
 });
