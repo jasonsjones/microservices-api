54c7980c5fed2bce79ced211c86c5c14bd2d74fd
Jason <jasonjones@salesforce.com>
2018-02-23 16:24:47 -0800

    Refactor test:api script to test:unit

---
+++ src/utils/testSetup.js
@@ -0,0 +1,3 @@
+process.env.NODE_ENV = 'test';
+
+console.log(`*** Running in ${process.env.NODE_ENV} mode`);
--- src/utils/testSetup.test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-
-describe("Test Environment", function () {
-    it("sets the node environment to test", function () {
-        expect(process.env.NODE_ENV).to.equal("test");
-    });
-});
