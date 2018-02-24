873f16e2674373a0a20178fa451a88499098e29e
Jason <jasonjones@salesforce.com>
2018-02-23 16:45:58 -0800

    Add new avatar routes

--- src/avatar/avatar.routes.js
+++ src/avatar/avatar.routes.js
@@ -1,9 +1,9 @@
-// import multer from 'multer';
+import multer from 'multer';
 import * as AvatarController from './avatar.controller';
 
 export default (app) => {
 
-    // const upload = multer({dest: './uploads/'});
+    const upload = multer({dest: './uploads/'});
 
     app.route('/api/avatars')
         .get((req, res) => {
@@ -30,6 +30,30 @@ export default (app) => {
         })
         .delete((req, res) => {
             AvatarController.deleteAvatar(req)
+                .then(response => res.json(response))
+                .catch(err => {
+                    res.status(500);
+                    res.json(err);
+                });
+        });
+
+    app.route('/api/avatar/default/:index')
+        .get((req, res) => {
+            AvatarController.getDefaultAvatar(req)
+                .then(response => {
+                    res.contentType(response.contentType);
+                    res.write(response.payload);
+                    res.end();
+                })
+                .catch(err => {
+                    res.status(500);
+                    res.json(err);
+                });
+        });
+
+    app.route('/api/avatar/default')
+        .post(upload.single('avatar'), (req, res) => {
+            AvatarController.uploadDefaultAvatar(req)
                 .then(response => res.json(response))
                 .catch(err => {
                     res.status(500);
