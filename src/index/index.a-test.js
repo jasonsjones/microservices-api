b1fbc1031f9bb62545d953aaea5707a67e395cd7
Jason <jasonjones@salesforce.com>
2018-02-24 19:44:42 -0800

    Remove db dependency

--- src/index/index.a-test.js
+++ src/index/index.a-test.js
@@ -2,29 +2,14 @@ import { expect } from 'chai';
 import request from 'supertest';
 
 import app from '../config/app';
-import Config from '../config/config';
-import db from '../config/db';
-
-const env = process.env.NODE_ENV || "development";
-const config = Config[env];
-
-let dbConnection;
 
 describe('Index integration tests', () => {
-
-    before(() => {
-        dbConnection = db(config);
-    });
-
-    after(() => {
-        dbConnection.close();
-    });
 
     it('app is present', () => {
         expect(app).to.exist;
     });
 
-    it('returns status 200 and json payload', () => {
+    it('GET /api', () => {
         return request(app)
             .get('/api')
             .expect(200)
