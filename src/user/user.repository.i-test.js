4eb14fd1f8ee5f75d671e651df2e3bf778f6d236
Jason <jasonjones@salesforce.com>
2018-03-16 06:29:38 -0700

    Add integration tests for uploadUserAvatar()

--- src/user/user.repository.i-test.js
+++ src/user/user.repository.i-test.js
@@ -1,7 +1,21 @@
+import fs from 'fs';
 import { expect } from 'chai';
 
 import * as Repository from './user.repository';
 import { dbConnection, dropCollection } from '../utils/dbTestUtils';
+
+const users = [
+    {
+        name: 'Barry Allen',
+        email: 'barry@starlabs.com',
+        password: '123456'
+    },
+    {
+        name: 'Oliver Queen',
+        email: 'arrow@qc.com',
+        password: '123456'
+    }
+];
 
 const expectUserShape = res => {
     expect(res).to.have.property('_id');
@@ -13,9 +27,11 @@ const expectUserShape = res => {
     expect(res).to.have.property('createdAt');
     expect(res).to.have.property('updatedAt');
 };
+
 describe('User repository integration tests', () => {
     after(() => {
         dropCollection(dbConnection, 'users');
+        dropCollection(dbConnection, 'avatars');
     });
 
     context('signUpUser()', () => {
@@ -38,6 +54,55 @@ describe('User repository integration tests', () => {
                 .catch(error => {
                     expect(error).to.exist;
                     expect(error.message).to.contain('user data is required');
+                });
+        });
+    });
+
+    context('uploadUserAvatar()', () => {
+        const assetPath = `${__dirname}/../../assets`;
+        const avatarFile = `${assetPath}/male3.png`;
+        const avatar = {
+            originalName: 'male3.png',
+            mimetype: 'image/png',
+            size: fs.statSync(avatarFile).size,
+            path: avatarFile
+        };
+
+        let userId;
+        before(() => {
+            return Repository.signUpUser(users[1])
+                .then(response => {
+                    userId = response._id;
+                });
+        });
+
+        after(() => {
+            dropCollection(dbConnection, 'users');
+            dropCollection(dbConnection, 'avatars');
+        });
+
+        it('uploads a custom avatar and associates with the user', () => {
+            return Repository.uploadUserAvatar(userId, avatar, false)
+                .then(response => {
+                    expectUserShape(response);
+                    expect(response.avatarUrl).to.not.contain('default');
+                    expect(response.avatar).to.exist;
+                });
+        });
+
+        it('returns an error if a user id is not provided', () => {
+            return Repository.uploadUserAvatar(null, avatar, false)
+                .catch(error => {
+                    expect(error).to.exist;
+                    expect(error.message).to.contain('user id is required');
+                });
+        });
+
+        it('returns an error if the avatar file is not provided', () => {
+            return Repository.uploadUserAvatar(userId, null, false)
+                .catch(error => {
+                    expect(error).to.exist;
+                    expect(error.message).to.contain('avatar file is required');
                 });
         });
     });
