9fe481e8b5f6b045cd91f0b6832f5e6f85aee5bc
Jason <jasonjones@salesforce.com>
2018-04-17 11:31:57 -0700

    Add avatar acceptance test

--- src/avatar/avatar.a-test.js
+++ src/avatar/avatar.a-test.js
@@ -3,14 +3,8 @@ import request from 'supertest';
 
 import app from '../config/app';
 import { expectAvatarShape } from '../utils/avatarTestUtils';
+import { expectJSONShape } from '../utils/testUtils';
 import { dbConnection, dropCollection } from '../utils/dbTestUtils';
-
-const expectJSONShape = json => {
-    expect(json).to.be.an('Object');
-    expect(json).to.have.property('success');
-    expect(json).to.have.property('message');
-    expect(json).to.have.property('payload');
-};
 
 describe.only('Avatar acceptence tests', () => {
 
@@ -25,9 +19,10 @@ describe.only('Avatar acceptence tests', () => {
                 .attach('avatar', `${__dirname}/../../assets/sfdc_default_avatar.png`)
                 .expect(200)
                 .then(res => {
-                    expectJSONShape(res.body, true);
+                    expectJSONShape(res.body);
                     expect(res.body.success).to.be.true;
-                    expectAvatarShape(res.body.payload);
+                    expectAvatarShape(res.body.payload, true);
+                    expect(res.body.payload.defaultImg).to.be.true;
                 });
         });
 
@@ -51,6 +46,61 @@ describe.only('Avatar acceptence tests', () => {
                     expect(res.body).to.have.property('message');
                     expect(res.body).to.have.property('error');
                     expect(res.body.success).to.be.false;
+                });
+        });
+    });
+
+    context('gets and deletes a user\'s custom avatar', () => {
+        let avatarId;
+
+        before(() => {
+            dropCollection(dbConnection, 'users');
+
+            return request(app)
+                .post('/api/signup')
+                .send({
+                    'name': 'Oliver Queen',
+                    'email': 'oliver@qc.com',
+                    'password': '123456'
+                })
+                .expect(200)
+                .then(res => {
+                    const userId = res.body.payload.user._id;
+                    return request(app)
+                        .post(`/api/user/${userId}/avatar`)
+                        .attach('avatar', `${__dirname}/../../assets/male3.png`)
+                        .expect(200)
+                        .then(res => {
+                            let parts = res.body.payload.user.avatarUrl.split('/');
+                            avatarId = parts[parts.length - 1];
+                        });
+                });
+        });
+
+        after(() => {
+            dropCollection(dbConnection, 'avatars');
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('GET /api/avatar/:id', () => {
+            return request(app)
+                .get(`/api/avatar/${avatarId}`)
+                .expect(200)
+                .then(res => {
+                    expect(res.body).to.exist;
+                    expect(typeof res.body === 'object').to.be.true;
+                });
+        });
+
+        it('DELETE /api/avatar/:id', () => {
+            return request(app)
+                .delete(`/api/avatar/${avatarId}`)
+                .expect(200)
+                .then(res => {
+                    expectJSONShape(res.body);
+                    expect(res.body.success).to.be.true;
+                    expectAvatarShape(res.body.payload, true);
+                    expect(res.body.payload._id).to.equal(avatarId);
                 });
         });
     });
