8c20ff14872f73c6e84db7e6d62cf64792424100
Jason Jones <jsjones96@gmail.com>
2018-05-18 06:15:18 -0700

    Update config with sane defaults

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,10 @@ import configDev from './configDev';
 import configTest from './configTest';
 dotenv.config();
 
+const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3000;
 const token_secret = process.env.JWT_SECRET;
 const session_secret = process.env.SESSION_SECRET;
-
-const env = process.env.NODE_ENV || 'development';
 
 let configBase = {
     version: '0.2.4',
@@ -15,14 +14,17 @@ let configBase = {
     port,
     token_secret,
     session_secret,
-    baseUrl: 'http://localhost'
+    baseUrl: 'http://localhost',
+    dbUrl: 'mongodb://mongo/sandboxapi-default'
 };
 
 let config;
 if (configBase.env === 'development') {
-    config = Object.assign(configBase, configDev);
+    config = Object.assign({}, configBase, configDev);
 } else if (configBase.env === 'testing') {
-    config = Object.assign(configBase, configTest);
+    config = Object.assign({}, configBase, configTest);
+} else {
+    config = Object.assign({}, configBase);
 }
 
 export default config;
