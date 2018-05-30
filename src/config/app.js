2ae73f32a8ee1e0f530337021dcf9dcfea4ed338
Jason Jones <jsjones96@gmail.com>
2018-05-30 06:54:49 -0700

    Refactor app middleware to module

--- src/config/app.js
+++ src/config/app.js
@@ -1,48 +1,19 @@
 import debug from 'debug';
 import express from 'express';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import session from 'express-session';
 import passport from 'passport';
-import cors from 'cors';
 
 import config from './config';
 import passportConfig from './passport';
+import applyMiddleware from './middleware';
 import registerRouters from './routes';
-
-const applyMiddleware = app => {
-    app.use(express.static('public'));
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    if (config.env === 'development') {
-        app.use(morgan('dev'));
-    }
-    app.use(cors());
-
-    app.use(
-        session({
-            cookie: {
-                secure: false
-            },
-            secret: config.session_secret,
-            resave: false,
-            saveUninitialized: true
-        })
-    );
-
-    app.use(passport.initialize());
-    app.use(passport.session());
-};
 
 const log = debug('app');
 log(`Running in ${config.env} mode`);
 
 const app = express();
 
-app.set('view engine', 'ejs');
 passportConfig(passport);
-applyMiddleware(app);
+applyMiddleware(app, passport);
 registerRouters(app, passport);
 
 export default app;
