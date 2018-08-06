d6f14f863dae1507b616475a0421bb45dc92d1cc
Jason Jones <jsjones96@gmail.com>
2018-08-06 11:04:58 -0700

    Add nodemailer unit tests

--- src/common/mailer.test.js
+++ src/common/mailer.test.js
@@ -1,10 +1,34 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
+import nodemailer from 'nodemailer';
 
 import { getMailTransporter } from './mailer';
 
 describe('Mailer', () => {
+    let transporter, nodemailerSpy;
+
+    before(() => {
+        nodemailerSpy = sinon.spy(nodemailer, 'createTransport');
+        transporter = getMailTransporter();
+    });
+
+    after(() => {
+        nodemailerSpy.restore();
+    });
+
     it('returns a mail transporter', () => {
-        const transporter = getMailTransporter();
         expect(transporter).to.exist;
+    });
+
+    it('calls nodemailer.createTrasnport() only once', () => {
+        expect(nodemailerSpy.calledOnce).to.be.true;
+    });
+
+    it('subsequent calls return cached transporter', () => {
+        const transporter2 = getMailTransporter();
+        // nodemailer.createTransport() should not be called again
+        // calledOnce still should be true
+        expect(nodemailerSpy.calledOnce).to.be.true;
+        expect(nodemailerSpy.calledTwice).to.be.false;
     });
 });
