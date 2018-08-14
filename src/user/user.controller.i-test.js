efed16ef0e9460ab9e2275d9ba62ea2052056f1b
Jason Jones <jsjones96@gmail.com>
2018-08-14 12:47:31 -0700

    Refactor user controller integration tests

--- src/user/user.controller.i-test.js
+++ src/user/user.controller.i-test.js
@@ -64,18 +64,23 @@ const expectErrorResponse = (errorResponse, errMsg) => {
 };
 
 describe('User controller integration tests', () => {
+    before(() => {
+        dropCollection(dbConnection, 'users');
+        dropCollection(dbConnection, 'avatars');
+    });
+
     context('createUser()', () => {
-        after(() => {
+        afterEach(() => {
             dropCollection(dbConnection, 'users');
         });
 
         it('returns error payload if the user data is not provided', () => {
             return Controller.createUser().catch(error => {
-                expectErrorResponse(error, 'request parameter is required');
+                expectErrorResponse(error, 'unable to create new user; user data is required');
             });
         });
 
-        it('creates a new user', () => {
+        it('resolves with user data and token for newly created user', () => {
             let req = {
                 body: users[0]
             };
@@ -89,6 +94,131 @@ describe('User controller integration tests', () => {
         });
     });
 
+    context('getUsers()', () => {
+        afterEach(() => {
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('returns all the users', () => {
+            return Controller.createUser({ body: users[0] })
+                .then(() => Controller.createUser({ body: users[1] }))
+                .then(() => Controller.getUsers())
+                .then(response => {
+                    expect(response).to.have.property('success');
+                    expect(response).to.have.property('message');
+                    expect(response).to.have.property('payload');
+                    expect(response.success).to.be.true;
+                    expect(response.payload.users).to.be.an('array');
+                    expectUserShape(response.payload.users[0]);
+                    expectUserShape(response.payload.users[1]);
+                });
+        });
+    });
+
+    context('getUser()', () => {
+        afterEach(() => {
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('returns a payload with the user with the given id', () => {
+            return Controller.createUser({ body: users[1] })
+                .then(response => response.payload.user._id)
+                .then(id => Controller.getUser({ params: { id } }))
+                .then(response => {
+                    expect(response).to.have.property('success');
+                    expect(response).to.have.property('message');
+                    expect(response).to.have.property('payload');
+                    expect(response.success).to.be.true;
+                });
+        });
+
+        it('returns a payload with the user and includes the avatar model', () => {
+            const avatar = getCopyOfAvatar();
+            return Controller.createUser({ body: users[1] })
+                .then(response => response.payload.user._id)
+                .then(id => Controller.uploadUserAvatar({ params: { id }, file: avatar }))
+                .then(response =>
+                    Controller.getUser({
+                        params: { id: response.payload.user._id },
+                        query: { includeAvatar: 'true' }
+                    })
+                )
+                .then(response => {
+                    expect(response).to.have.property('success');
+                    expect(response).to.have.property('message');
+                    expect(response).to.have.property('payload');
+                    expect(response.success).to.be.true;
+                    expect(response.payload.user.avatar).to.exist;
+                    expect(response.payload.user.avatar).to.be.an('object');
+                    expect(response.payload.user.avatar).to.have.property('_id');
+                    expect(response.payload.user.avatar).to.have.property('contentType');
+                    expect(response.payload.user.avatar).to.have.property('defaultImg');
+                });
+        });
+    });
+
+    context('unlinkSFDCAccount()', () => {
+        afterEach(() => {
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('returns an error if the user does not have a linked SFDC profile', () => {
+            return Controller.createUser({ body: users[1] })
+                .then(response => response.payload.user._id)
+                .then(id => Controller.getUser({ params: { id } }))
+                .then(response => Controller.unlinkSFDCAccount({ user: response.payload.user }))
+                .then(response => expectErrorResponse(response, 'error unlinking the user'));
+        });
+    });
+
+    context('updateUser()', () => {
+        afterEach(() => {
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('updates the user with the provided data', () => {
+            return Controller.createUser({ body: users[1] })
+                .then(response => response.payload.user._id)
+                .then(id =>
+                    Controller.updateUser({
+                        params: { id },
+                        body: {
+                            name: 'The Flash',
+                            email: 'flash@starlabs.com'
+                        }
+                    })
+                )
+                .then(response => {
+                    expect(response).to.have.property('success');
+                    expect(response).to.have.property('message');
+                    expect(response).to.have.property('payload');
+                    expect(response.success).to.be.true;
+                    expectClientJSONUserShape(response.payload.user);
+                    expect(response.payload.user.name).to.not.equal(users[0].name);
+                    expect(response.payload.user.email).to.not.equal(users[0].email);
+                });
+        });
+    });
+
+    context('deleteUser()', () => {
+        afterEach(() => {
+            dropCollection(dbConnection, 'users');
+        });
+
+        it('returns the a payload with the user that was just deleted', () => {
+            return Controller.createUser({ body: users[1] })
+                .then(response => response.payload.user._id)
+                .then(id => Controller.deleteUser({ params: { id } }))
+                .then(response => {
+                    expect(response).to.have.property('success');
+                    expect(response).to.have.property('message');
+                    expect(response).to.have.property('payload');
+                    expect(response.success).to.be.true;
+                    expectUserShape(response.payload.user);
+                });
+        });
+    });
+
     context('uploadUserAvatar()', () => {
         let barryId;
         before(() => {
@@ -96,7 +226,6 @@ describe('User controller integration tests', () => {
                 barryId = response.payload.user._id;
             });
         });
-
         after(() => {
             dropCollection(dbConnection, 'users');
             dropCollection(dbConnection, 'avatars');
@@ -195,168 +324,6 @@ describe('User controller integration tests', () => {
             };
             return Controller.changePassword(req).catch(error => {
                 expectErrorResponse(error, 'request body is required');
-            });
-        });
-    });
-
-    context('user fetching and mutating related tests', () => {
-        let barryId;
-        before(() => {
-            return Controller.createUser({ body: users[0] }).then(response => {
-                barryId = response.payload.user._id;
-                Controller.createUser({ body: users[1] });
-            });
-        });
-
-        after(() => {
-            dropCollection(dbConnection, 'users');
-            dropCollection(dbConnection, 'avatars');
-        });
-
-        context('getUsers()', () => {
-            it('returns all the users', () => {
-                return Controller.getUsers().then(response => {
-                    expect(response).to.have.property('success');
-                    expect(response).to.have.property('message');
-                    expect(response).to.have.property('payload');
-                    expect(response.success).to.be.true;
-                    expect(response.payload.users).to.be.an('array');
-                    expectUserShape(response.payload.users[0]);
-                    expectUserShape(response.payload.users[1]);
-                });
-            });
-        });
-
-        context('getUser()', () => {
-            it('returns a payload with the user of the given id', () => {
-                return Controller.getUser({ params: { id: barryId } }).then(response => {
-                    expect(response).to.have.property('success');
-                    expect(response).to.have.property('message');
-                    expect(response).to.have.property('payload');
-                    expect(response.success).to.be.true;
-                });
-            });
-
-            it('returns a payload with the user and includes the avatar model', () => {
-                const avatar = getCopyOfAvatar();
-                const req = {
-                    params: {
-                        id: barryId
-                    },
-                    file: avatar,
-                    query: {
-                        includeAvatar: 'true'
-                    }
-                };
-                return Controller.uploadUserAvatar(req)
-                    .then(() => Controller.getUser(req))
-                    .then(response => {
-                        expectUserShape(response.payload.user);
-                        expect(response.payload.user.avatar).to.exist;
-                        expect(response.payload.user.avatar).to.be.an('object');
-                        expect(response.payload.user.avatar).to.have.property('_id');
-                        expect(response.payload.user.avatar).to.have.property('contentType');
-                        expect(response.payload.user.avatar).to.have.property('defaultImg');
-                    });
-            });
-
-            it('returns error payload if the request is not provided', () => {
-                return Controller.getUser().catch(error => {
-                    expectErrorResponse(error, 'request parameter is required');
-                });
-            });
-        });
-
-        context('unlinkSFDCAccount()', () => {
-            it('returns an error if the user does not have a linked SFDC profile', () => {
-                let req = {
-                    params: {
-                        id: barryId
-                    }
-                };
-                return Controller.getUser(req)
-                    .then(response => {
-                        req.user = response.payload.user;
-                        return Controller.unlinkSFDCAccount(req);
-                    })
-                    .then(response => {
-                        expectErrorResponse(response, 'error unlinking the user');
-                    });
-            });
-        });
-
-        context('updateUser()', () => {
-            it('updates the user with the provided data', () => {
-                const req = {
-                    params: {
-                        id: barryId
-                    },
-                    body: {
-                        name: 'The Flash',
-                        email: 'flash@starlabs.com'
-                    }
-                };
-                return Controller.updateUser(req).then(response => {
-                    expect(response).to.have.property('success');
-                    expect(response).to.have.property('message');
-                    expect(response).to.have.property('payload');
-                    expect(response.success).to.be.true;
-                    expectClientJSONUserShape(response.payload.user);
-                    expect(response.payload.user.name).to.not.equal(users[0].name);
-                    expect(response.payload.user.email).to.not.equal(users[0].email);
-                });
-            });
-
-            it('returns an error if the request parameter is not provided', () => {
-                return Controller.updateUser().catch(error => {
-                    expectErrorResponse(error, 'request parameter is required');
-                });
-            });
-
-            it('returns an error if the user id is not provided', () => {
-                const req = {
-                    body: {
-                        name: 'The Flash'
-                    }
-                };
-                return Controller.updateUser(req).catch(error => {
-                    expectErrorResponse(error, 'user id is required');
-                });
-            });
-
-            it('returns an error if the new user data is not provided', () => {
-                const req = {
-                    params: {
-                        id: barryId
-                    }
-                };
-                return Controller.updateUser(req).catch(error => {
-                    expectErrorResponse(error, 'user data is required');
-                });
-            });
-        });
-
-        context('deleteUser()', () => {
-            it('returns error payload if the request is not provided', () => {
-                return Controller.deleteUser().catch(error => {
-                    expectErrorResponse(error, 'request parameter is required');
-                });
-            });
-
-            it('returns the a payload with the user that was just deleted', () => {
-                const req = {
-                    params: {
-                        id: barryId
-                    }
-                };
-                return Controller.deleteUser(req).then(response => {
-                    expect(response).to.have.property('success');
-                    expect(response).to.have.property('message');
-                    expect(response).to.have.property('payload');
-                    expect(response.success).to.be.true;
-                    expectUserShape(response.payload.user);
-                    expect(response.payload.user._id).to.eql(barryId);
-                });
             });
         });
     });
