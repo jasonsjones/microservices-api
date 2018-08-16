fc0a67a1c324c42a6219f249f565f3a6eb0d97e2
Jason Jones <jsjones96@gmail.com>
2018-08-16 13:49:21 -0700

    Add test scripts to simplify things

---
+++ scripts/test-acceptance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# colors
+RESTORE='\033[0m'
+CYAN='\033[00;36m'
+SRC_SETUP='src/utils/dbTestSetup.js'
+
+if [ -z $1 ]
+    then
+        SRC='src/**/*.a-test.js'
+        echo -e "\nNo feature or module supplied\n"
+    else
+        SRC="src/**/$1.a-test.js"
+        echo -e "${CYAN}*** Running acceptance tests for $1 feature and/or module ***${RESTORE}"
+fi
+
+NODE_ENV=test DEBUG=db:connection,test npx mocha --exit $SRC_SETUP $SRC
+echo "Tests complete!"
