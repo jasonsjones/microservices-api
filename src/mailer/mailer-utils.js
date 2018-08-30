f2409276a55d88745423e880a6ada6c1dd4f6376
Jason Jones <jsjones96@gmail.com>
2018-08-29 20:06:39 -0700

    Add sendEmailVerificationEmail() and minor refactor

--- src/mailer/mailer-utils.js
+++ src/mailer/mailer-utils.js
@@ -7,9 +7,10 @@ import * as templates from '../mailer/email-templates';
 
 const log = debug('mailer');
 
-export const sendPasswordResetEmail = (user, resetUrl) => {
+export const sendPasswordResetEmail = user => {
     return new Promise((resolve, reject) => {
-        let mailOptions = getMailOptions(user, resetUrl);
+        const resetUrl = `${config.url}/reset/${user.passwordResetToken}`;
+        let mailOptions = getMailOptionsForPasswordReset(user, resetUrl);
         let transporter = getMailTransporter();
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
@@ -18,17 +19,44 @@ export const sendPasswordResetEmail = (user, resetUrl) => {
             log('Message sent: %s', info.messageId);
             // Preview only available when sending through an Ethereal account
             log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            resolve({ email: user.email, info });
+            resolve({ user, info });
         });
     });
 };
 
-const getMailOptions = (user, resetUrl) => {
+export const sendEmailVerificationEmail = user => {
+    return new Promise((resolve, reject) => {
+        const verifyUrl = `${config.url}/reset/${user.emailVerificationToken}`;
+        let mailOptions = getMailOptionsForEmailVerification(user, verifyUrl);
+        let transporter = getMailTransporter();
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                reject(error);
+            }
+            log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            resolve({ user, info });
+        });
+    });
+};
+
+const getMailOptionsForPasswordReset = (user, url) => {
     return {
         from: config.emailAddr,
         to: `"${user.name}" <${user.email}>`, // list of receivers
         subject: 'Password Reset', // Subject line
-        text: templates.passwordResetEmailPlainText(resetUrl),
-        html: templates.passwordResetEmailHTML(resetUrl)
+        text: templates.passwordResetEmailPlainText(url),
+        html: templates.passwordResetEmailHTML(url)
+    };
+};
+
+const getMailOptionsForEmailVerification = (user, url) => {
+    return {
+        from: config.emailAddr,
+        to: `"${user.name}" <${user.email}>`, // list of receivers
+        subject: 'Email Verification', // Subject line
+        text: 'text email',
+        html: `<p>html email</p><p>${url}</p>`
     };
 };
