68927c1390d7f5cdb1b8a72af1402f64bea7011b
Jason Jones <jsjones96@gmail.com>
2018-09-17 16:07:16 -0700

    Add nodemailer createTestAccount stub

--- src/user/user.controller.i-test.js
+++ src/user/user.controller.i-test.js
@@ -368,6 +368,19 @@ describe('User controller integration tests', () => {
         });
 
         it('sends an email to the user with a link to reset password', () => {
+            const mockTestAccountResponse = {
+                user: 'test-account@ethereal.email',
+                pass: 'u6XKFA5qGUjhgzrBaw',
+                smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+                imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+                pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
+                web: 'https://ethereal.email'
+            };
+            const createTestAccountStub = sinon.stub(nodemailer, 'createTestAccount');
+            const testAccountFake = cb => {
+                cb(null, mockTestAccountResponse);
+            };
+            createTestAccountStub.callsFake(testAccountFake);
             let mockTransporter = {
                 sendMail: (data, cb) => {
                     cb(null, {
@@ -391,6 +404,7 @@ describe('User controller integration tests', () => {
                     expect(response.payload).to.have.property('info');
                     expect(response.success).to.be.true;
                     mailerStub.restore();
+                    createTestAccountStub.restore();
                 });
             });
         });
