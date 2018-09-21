59d6890d9079cb0e707e9b9d00f813e1fc2918c7
Jason Jones <jsjones96@gmail.com>
2018-09-21 13:51:31 -0700

    Add comment describing how to debug tests

--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -13,6 +13,9 @@ if [ -z $1 ]
         echo -e "${CYAN}*** Running unit tests for $1 feature and/or module ***${RESTORE}"
 fi
 
+# pass along a --inspect-brk to the 'mocha' cmd to trigger a breakpoint at the start of runnning the tests
+# then go to chrome://inspect in the browser to find the remote session in order to debug the tests
+
 NODE_ENV=test DEBUG=test,mailer npx mocha --exit $SRC_SETUP $SRC
 TEST_STATUS=$?
 
