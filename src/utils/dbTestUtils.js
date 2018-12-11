a1151ea7505043eb1513d976dc84b62af3cad391
Jason Jones <jsjones96@gmail.com>
2018-12-11 09:34:32 -0800

    test: add deleteCollection util method

--- src/utils/dbTestUtils.js
+++ src/utils/dbTestUtils.js
@@ -18,6 +18,19 @@ export const dropCollection = (connection, collectionName, cb) => {
     });
 };
 
+export const deleteCollection = async (connection, model, collectionName) => {
+    let count = await model
+        .find()
+        .countDocuments()
+        .exec();
+
+    if (count > 0) {
+        return await connection.dropCollection(collectionName);
+    } else {
+        return await Promise.resolve();
+    }
+};
+
 export const addDefaultAvatar = () => {
     const assetPath = `${__dirname}/../../assets`;
     const avatarFile = `${assetPath}/sfdc_default_avatar.png`;
